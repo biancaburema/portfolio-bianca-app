5fefd374556c0b2d5fd413dd9c4ee4b73dda6d4c
Bianca <biancaburema@gmail.com>
2022-09-15 17:37:27 +0200

    First look at the homepage

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'portfolio-bianca-app';
+  title = 'Portfolio van Bianca Burema';
 }
